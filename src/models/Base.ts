e2e133fc73fbf5d3e651527df17192b69c9818cf
MakeShiftArtist <lrmcbride4@gmail.com>
2022-09-16 23:57:29 -0500

    Feat: Add Base class

---
+++ src/models/Base.ts
@@ -0,0 +1,25 @@
+/**
+ * Base Class that each object will extend
+ * @template Data Interface for the Raw Data returned from the API
+ */
+class Base<Data extends any> {
+	/**
+	 * The Data passed to the constructor
+	 */
+	#data: Data;
+	/**
+	 * @param data The data to create the instance with
+	 */
+	constructor(data: Data) {
+		this.#data = data;
+	}
+
+	/**
+	 * The raw data for the object in JSON like structure
+	 */
+	get raw(): Data {
+		return this.#data;
+	}
+}
+
+export default Base;
