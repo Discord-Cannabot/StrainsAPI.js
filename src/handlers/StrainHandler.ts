66a6a0968afe22db5c02bdf913a8a66eac7f2f8b
MakeShiftArtist <lrmcbride4@gmail.com>
2022-09-17 22:30:00 -0500

    Feat: Improve docs and config for StrainHandler

--- src/handlers/StrainHandler.ts
+++ src/handlers/StrainHandler.ts
@@ -1,23 +1,28 @@
 import { RawStrain, Strain, StrainRace } from "../models/Strain";
 
 import BaseHandler from "./BaseHandler";
-import Client from "../models/Client";
+import Client from "../client/BaseClient";
+import { ICachingOptions } from "node-ts-cache";
 
-export enum SEARCH_BY {
+export enum STRAIN_SEARCH_BY {
 	Name = "name",
 	Id = "id",
 	Race = "race",
 	Rating = "rating",
 }
 
-export type SearchBy = `${SEARCH_BY}`;
+export type StrainSearchBy = `${STRAIN_SEARCH_BY}`;
 
 /**
  * Handles Strains for the Client with a cache
  */
 class StrainHandler extends BaseHandler<typeof Strain> {
-	constructor(client: Client) {
-		super(client, Strain);
+	/**
+	 * @param client Client attached to the handler
+	 * @param options Options for the cache
+	 */
+	constructor(client: Client, options?: ICachingOptions) {
+		super(client, Strain, options);
 	}
 
 	/**
@@ -44,7 +49,7 @@ class StrainHandler extends BaseHandler<typeof Strain> {
 	 * @param force Force an API request and update the cache
 	 * @returns Array of Strains
 	 */
-	async search(by: SearchBy, query: string, force: boolean = false) {
+	async search(by: StrainSearchBy, query: string, force: boolean = false) {
 		let strains = await this.cache.getItem<Strain[]>(query);
 		if (strains && !force) return strains;
 		strains = [];
@@ -68,7 +73,7 @@ class StrainHandler extends BaseHandler<typeof Strain> {
 	 * @param force Force an API request and update the cache
 	 */
 	async byName(name: string, force: boolean = false): Promise<Strain[]> {
-		return await this.search(SEARCH_BY.Name, name, force);
+		return await this.search(STRAIN_SEARCH_BY.Name, name, force);
 	}
 
 	/**
@@ -77,7 +82,7 @@ class StrainHandler extends BaseHandler<typeof Strain> {
 	 * @param force Force an API request and update the cache
 	 */
 	async byId(id: number, force: boolean = false): Promise<Strain[]> {
-		return await this.search(SEARCH_BY.Id, `${id}`, force);
+		return await this.search(STRAIN_SEARCH_BY.Id, `${id}`, force);
 	}
 
 	/**
@@ -86,7 +91,7 @@ class StrainHandler extends BaseHandler<typeof Strain> {
 	 * @param force Force an API request and update the cache
 	 */
 	async byRating(rating: number, force: boolean = false): Promise<Strain[]> {
-		return await this.search(SEARCH_BY.Rating, `${rating}`, force);
+		return await this.search(STRAIN_SEARCH_BY.Rating, `${rating}`, force);
 	}
 
 	/**
@@ -95,7 +100,7 @@ class StrainHandler extends BaseHandler<typeof Strain> {
 	 * @param force Force an API request and update the cache
 	 */
 	async byRace(race: StrainRace, force: boolean = false): Promise<Strain[]> {
-		return await this.search(SEARCH_BY.Race, race, force);
+		return await this.search(STRAIN_SEARCH_BY.Race, race, force);
 	}
 }
 
