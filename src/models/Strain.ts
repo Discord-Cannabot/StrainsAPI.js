7a1352f4f22f6df124e7bd867a4222d32e4eef34
MakeShiftArtist <lrmcbride4@gmail.com>
2022-09-17 22:32:56 -0500

    Feat: Moved created_at and updated_at to Base

--- src/models/Strain.ts
+++ src/models/Strain.ts
@@ -95,34 +95,6 @@ export class Strain extends Base<RawStrain> {
 	get rating(): number {
 		return Number.parseFloat(this.raw.rating);
 	}
-
-	/**
-	 * Timestamp of when the strain was created in the Database
-	 */
-	get createdAtTimestamp(): number {
-		return Date.parse(this.raw.created_at);
-	}
-
-	/**
-	 * Timestamp of when the strain was last updated in the Database
-	 */
-	get updatedAtTimestamp(): number {
-		return Date.parse(this.raw.updated_at);
-	}
-
-	/**
-	 * When the strain was created in the Database
-	 */
-	get createdAt(): Date {
-		return new Date(this.createdAtTimestamp);
-	}
-
-	/**
-	 * When the strain was updated in the Database
-	 */
-	get updatedAt(): Date {
-		return new Date(this.updatedAtTimestamp);
-	}
 }
 
 export default Strain;
