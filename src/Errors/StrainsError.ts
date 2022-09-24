342209de8e073b61e95f83c3d9d862e95e7f8033
MakeShiftArtist <lrmcbride4@gmail.com>
2022-09-24 18:47:03 -0500

    Feat: Added built in error handling to BaseClient

---
+++ src/Errors/StrainsError.ts
@@ -0,0 +1,56 @@
+import { AxiosError } from "axios";
+
+/**
+ * Raw strain data returned from StrainsAPI
+ */
+export interface StrainsAPIError {
+	code: string;
+	message: string;
+	status: number;
+}
+
+/**
+ * Represents an Error thrown by StrainsAPI
+ */
+export class StrainsError extends Error {
+	readonly #code: string;
+
+	readonly #status: number;
+
+	/**
+	 * @param error Axios Error thrown when the API returns an Error
+	 */
+	constructor(error: AxiosError<StrainsAPIError>) {
+		let data = error.response!.data;
+		super(data.message);
+
+		this.#code = data.code;
+
+		this.#status = data.status;
+
+		Error.captureStackTrace(this.constructor);
+	}
+
+	/**
+	 * The name of the error
+	 */
+	override get name() {
+		return `${this.constructor.name} - ${this.code} [${this.status}]`;
+	}
+
+	/**
+	 * The error code returned by the API
+	 */
+	get code() {
+		return this.#code;
+	}
+
+	/**
+	 * Response status code
+	 */
+	get status() {
+		return this.#status;
+	}
+}
+
+export default StrainsError;
