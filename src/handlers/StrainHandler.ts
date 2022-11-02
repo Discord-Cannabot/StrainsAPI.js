e1d46058e331611c6b0df8658fd9ec720118e511
makeshiftartist <lrmcbride4@gmail.com>
2022-11-02 00:00:35 -0500

    Feat: Add additional filter params and methods

--- src/handlers/StrainHandler.ts
+++ src/handlers/StrainHandler.ts
@@ -3,12 +3,41 @@ import { RawStrain, Strain, StrainRaceType } from "../models/Strain";
 import BaseHandler from "./BaseHandler";
 import Client from "../client/BaseClient";
 import { ICachingOptions } from "node-ts-cache";
+import { assert } from "console";
 
 export interface StrainSearchParams {
 	name?: string;
 	description?: string;
 	race?: StrainRaceType;
 	rating?: number;
+	minRating?: number;
+	maxRating?: number;
+	effects?: string[];
+	flavours?: string[];
+	flavors?: string[];
+}
+
+/**
+ * Validates that a parent string array includes every string from the child array
+ * @param array Parent array
+ * @param values Child array
+ * @param caseSensitive Should the array (Default: false)
+ * @returns boolean
+ */
+export function includesAll(
+	array: string[],
+	values: string[],
+	caseSensitive: boolean = false
+): boolean {
+	for (const value of values) {
+		if (
+			array.findIndex((item) =>
+				caseSensitive ? item === value : new RegExp(`^${value}$`, "i").test(item)
+			) === -1
+		)
+			return false;
+	}
+	return true;
 }
 
 /**
@@ -27,9 +56,8 @@ export class StrainHandler extends BaseHandler<typeof Strain> {
 	/**
 	 * Returns all strains in the Database
 	 * @param force Force an API request and update the cache
-	 * @returns Array of Strains
 	 */
-	async all(force: boolean = false) {
+	async all(force: boolean = false): Promise<Strain[]> {
 		const strains = (await this.get<Strain[]>("all")) ?? [];
 		if (strains.length > 0 && !force) return strains;
 		const { data } = await this.client.instance.get<RawStrain[]>("strains");
@@ -41,49 +69,78 @@ export class StrainHandler extends BaseHandler<typeof Strain> {
 		return strains;
 	}
 
-	// /**
-	//  * Search for a strain by keys
-	//  * @param by What to search by
-	//  * @param query The query for the search
-	//  * @param force Force an API request and update the cache
-	//  * @returns Array of Strains
-	//  */
-	// async search(by: StrainSearchTerm, query: string, force: boolean = false) {
-	// 	let strains = await this.cache.getItem<Strain[]>(query);
-	// 	if (strains && !force) return strains;
-	// 	strains = [];
-
-	// 	const { data } = await this.client.instance.get<RawStrain[]>(
-	// 		`strains/search/${by}/${query}`
-	// 	);
-
-	// 	for (const strain of data) {
-	// 		strains.push(new Strain(strain));
-	// 	}
-
-	// 	this.set(query, strains);
-	// 	return strains;
-	// }
-
 	/**
 	 * Filter strains using the API
 	 * @param params Params to filter strains for
+	 * @param params.name Name of the strain
+	 * @param params.race "indica" | "sativa" | "hybrid"
+	 * @param params.description A string value in the description to filter by
+	 * @param params.rating The exact rating of the strain (0-5). No higher, no lower
+	 * @param params.minRating The minimum rating for the strains (0-5)
+	 * @param params.maxRating The maximum rating for the strains (0-5)
+	 * @param params.effects Effects of the strain
+	 * @param params.flavours Flavours of the strain
+	 * @param params.flavors Flavors of the strain (Alias)
 	 * @param force Force an API request and update the cache
 	 */
-	async filter(params: StrainSearchParams, force: boolean = false) {
-		let strains = await this.cache.getItem<Strain[]>(JSON.stringify(params, null, 0));
-		if (strains && !force) return strains;
-		strains = [];
+	async filter(params: StrainSearchParams, force: boolean = false): Promise<Strain[]> {
+		let strains = await this.all(force);
+		if (!params) return strains;
+		const {
+			name,
+			description,
+			race,
+			rating,
+			minRating,
+			maxRating,
+			effects,
+			flavors,
+			flavours,
+		} = params;
 
-		const { data } = await this.client.instance.get<RawStrain[]>("strains/search", {
-			params,
-		});
-
-		for (const strain of data) {
-			strains.push(new Strain(strain));
+		if (name && typeof name === "string") {
+			const regex = new RegExp(name, "gi");
+			strains = strains.filter((strain) => regex.test(strain.name));
 		}
 
-		this.set(JSON.stringify(params, null, 0), strains, this.config);
+		if (typeof rating === "number") {
+			if (rating < 0 || rating > 5) return [];
+			strains = strains.filter((strain) => strain.rating === rating);
+		}
+
+		if (race && typeof race === "string") {
+			const regex = new RegExp(race, "gi");
+			strains = strains.filter((strain) => regex.test(strain.race));
+		}
+
+		if (description && typeof description === "string") {
+			const regex = new RegExp(description);
+			strains = strains.filter((strain) => regex.test(strain.description));
+		}
+
+		if (typeof maxRating === "number" && maxRating < 5) {
+			const max = maxRating; // to appease typesaftey in callback
+			strains = strains.filter((strain) => strain.rating <= max);
+		}
+
+		if (typeof minRating === "number" && minRating > 0) {
+			const min = minRating; // to appease typesaftey in callback
+			strains = strains.filter((strain) => strain.rating >= min);
+		}
+
+		if (effects) {
+			strains = strains.filter((strain) => includesAll(strain.effects, effects));
+		}
+
+		if (flavours || flavors) {
+			let both: string[] = [];
+			if (flavours) both = both.concat(flavours);
+			if (flavors) both = both.concat(flavors);
+			both = [...new Set(both)];
+
+			strains = strains.filter((strain) => includesAll(strain.effects, both));
+		}
+
 		return strains;
 	}
 
@@ -96,18 +153,9 @@ export class StrainHandler extends BaseHandler<typeof Strain> {
 		return await this.filter({ name }, force);
 	}
 
-	// /**
-	//  * Search for a strain by ID
-	//  * @param id Id of the strain
-	//  * @param force Force an API request and update the cache
-	//  */
-	// async byId(id: number, force: boolean = false): Promise<Strain[]> {
-	// 	return await this.search(SearchStrainBy.ID, `${id}`, force);
-	// }
-
 	/**
 	 * Search for a strain by rating
-	 * @param rating Rating of the Strain
+	 * @param rating Rating to filter (0-5)
 	 * @param force Force an API request and update the cache
 	 */
 	async byRating(rating: number, force: boolean = false): Promise<Strain[]> {
@@ -115,8 +163,8 @@ export class StrainHandler extends BaseHandler<typeof Strain> {
 	}
 
 	/**
-	 * Search for a strain by race
-	 * @param race Race to search for (`indica` | `sativa` | `hybrid`)
+	 * Filter strains by race
+	 * @param race Race to filter (`indica` | `sativa` | `hybrid`)
 	 * @param force Force an API request and update the cache
 	 */
 	async byRace(race: StrainRaceType, force: boolean = false): Promise<Strain[]> {
@@ -124,12 +172,68 @@ export class StrainHandler extends BaseHandler<typeof Strain> {
 	}
 
 	/**
-	 * Searches for strains based on data found in the description
-	 * @param description Description keywords to search for
+	 * Filter strains by description keywords
+	 * @param description Description keywords to filter by
 	 * @param force Force an API request and update the cache
 	 */
 	async byDescription(description: string, force: boolean = false): Promise<Strain[]> {
 		return await this.filter({ description }, force);
+	}
+
+	/**
+	 * Filters for strains by effects
+	 * @param effects Effects to filter
+	 * @param force Force an API request and update the cache
+	 */
+	async byEffects(
+		effects: string | string[],
+		force: boolean = false
+	): Promise<Strain[]> {
+		assert(
+			typeof effects === "string" || Array.isArray(effects),
+			"effects must be a string or array of strings"
+		);
+		return await this.filter(
+			{
+				effects: Array.isArray(effects) ? effects : [effects],
+			},
+			force
+		);
+	}
+
+	/**
+	 * Filter by strain flavours
+	 * @param flavours Flavours to filter
+	 * @param force Force an API request and update the cache
+	 * @alias byFlavors
+	 */
+	async byFlavours(
+		flavours: string | string[],
+		force: boolean = false
+	): Promise<Strain[]> {
+		assert(
+			typeof flavours === "string" || Array.isArray(flavours),
+			"effects must be a string or array of strings"
+		);
+		return await this.filter(
+			{
+				flavours: Array.isArray(flavours) ? flavours : [flavours],
+			},
+			force
+		);
+	}
+
+	/**
+	 * Filter by strain flavors
+	 * @param flavors Flavors to filter
+	 * @param force Force an API request and update the cache
+	 * @alias byFlavours
+	 */
+	async byFlavors(
+		flavors: string | string[],
+		force: boolean = false
+	): Promise<Strain[]> {
+		return await this.byFlavours(flavors, force);
 	}
 }
 
